5e1a1c34c1028d6847bf1ad6e4bde65d1d815b7b
mr-u0b0dy <63730630+mr-u0b0dy@users.noreply.github.com>
2026-03-21 13:11:09 +0530

    feat(examples): add Go coverage example project

---
+++ coverage-examples/go/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Add(2, 3):", Add(2, 3))
+	fmt.Println("Multiply(4, 5):", Multiply(4, 5))
+	fmt.Println("Divide(10, 2):", Divide(10, 2))
+	fmt.Println("Classify(7):", Classify(7))
+}
